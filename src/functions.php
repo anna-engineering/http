59fd7b02938d4b892a75ae5b0a5c3ded0c6f5f84
aaurizon <750048+aaurizon@users.noreply.github.com>
2025-04-20 16:01:48 +0200

    Add initial fetch function definition
    Introduced a new `fetch` function in `functions.php` with placeholders for URL, data, method, and headers. This serves as a starting point for implementing HTTP requests.

---
+++ src/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!function_exists('fetch'))
+{
+    // https://curl.se/docs/caextract.html
+    function fetch($url, string|null $data = null, string|null $method = null, $headers = [])
+    {
+    }
+}
