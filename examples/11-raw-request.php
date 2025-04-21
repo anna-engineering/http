5441943b0f1b50853150e33bd222a40004b05f40
aaurizon <750048+aaurizon@users.noreply.github.com>
2025-04-21 08:50:49 +0200

    Fix raw HTTP request formatting in example
    Updated the raw HTTP request in examples/11-raw-request.php to use proper CRLF line endings as required by the HTTP specification.

--- examples/11-raw-request.php
+++ examples/11-raw-request.php
@@ -2,12 +2,12 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$message = 'POST /users HTTP/1.1';
-$message.= 'Host: example.com';
-$message.= 'Content-Type: application/x-www-form-urlencoded';
-$message.= 'Content-Length: 50';
-$message.= '';
-$message.= 'name=FirstName%20LastName&email=bsmth%40example.com';
+$message = "POST /users HTTP/1.1\r\n";
+$message.= "Host: example.com\r\n";
+$message.= "Content-Type: application/x-www-form-urlencoded\r\n";
+$message.= "Content-Length: 50\r\n";
+$message.= "\r\n";
+$message.= "name=FirstName%20LastName&email=bsmth%40example.com";
 
 $request = \A\Http\Request::fromMessage($message);
 
