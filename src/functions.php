be3ecd1860a85053c93fab94ff9744fdd0559bea
aaurizon <750048+aaurizon@users.noreply.github.com>
2025-04-21 08:46:26 +0200

    Refactor fetch function and add fetch_stream
    Updated the fetch function to include type hints, return type, and improved implementation using Curl and PromiseProxy. Added a new fetch_stream function to support streaming responses with a customizable separator.

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,42 @@
 if (!function_exists('fetch'))
 {
     // https://curl.se/docs/caextract.html
-    function fetch($url, string|null $data = null, string|null $method = null, $headers = [])
+    /**
+     * @param string $url
+     * @param string|null $data
+     * @param string|null $method
+     * @param array $headers
+     * @return \A\Async\PromiseProxyInterface|\A\Http\Response
+     */
+    function fetch($url, string $data = '', string|null $method = '', $headers = []) : \A\Async\PromiseProxyInterface
     {
+        $curl = new \A\Http\Curl(new \A\Http\Request(
+            $method ?: 'GET',
+            $url,
+            \A\Http\Message::PROTOCOL_1_1,
+            new \A\Http\Headers($headers),
+            $data
+        ));
+
+        return new \A\Async\PromiseProxy(function () use ($curl) {
+            return $curl->exec();
+        });
+    }
+}
+
+if (!function_exists('fetch_stream'))
+{
+    // https://curl.se/docs/caextract.html
+    function fetch_stream($url, string|null $data = null, string|null $method = null, $headers = [], $separator = \A\Http\Message::LINEBREAK)
+    {
+        $curl = new \A\Http\Curl(new \A\Http\Request(
+                                     $method ?: 'GET',
+                                     $url,
+                                     \A\Http\Message::PROTOCOL_1_1,
+                                     new \A\Http\Headers($headers),
+                                     $data
+        ));
+
+        yield from $curl->stream($separator);
     }
 }
